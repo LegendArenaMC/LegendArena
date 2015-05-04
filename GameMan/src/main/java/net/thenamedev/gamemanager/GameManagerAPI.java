c125a96cf321bbf4695f72378006a3e5a1a91e11
ThePixelDev <6title78cp@gmail.com>
2015-05-04 15:49:09 -0600

    100th commit, fixing things. Yep, seems about right.

---
+++ GameMan/src/main/java/net/thenamedev/gamemanager/GameManagerAPI.java
@@ -0,0 +1,19 @@
+package net.thenamedev.gamemanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Internal API for the Game Manager utilities.<br><br>
+ *
+ * Created by thepixeldev on 5/4/15.
+ */
+public class GameManagerAPI {
+
+    private static List<Minigame> mg = new ArrayList<>();
+
+    protected static List<Minigame> minigames() {
+        return null;
+    }
+
+}
