66c0f03c2e5f9d8366785e3b277b74367c1d3ca1
ThePixelDev <6title78cp@gmail.com>
2015-08-08 01:47:24 -0600

    tweak the build script to upload artifacts if built from snapci

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
 if hash gradle 2>/dev/null; then
     gradle build shadowJar
 else
     ./gradlew build shadowJar --no-daemon
 fi
 
-#gradle build shadowJar || { ./gradlew build shadowJar }
-rm build/libs/`basename \`pwd\``.jar
+rm build/libs/LegendArena.jar
 mv build/libs/LegendArena-all.jar build/libs/LegendArena.jar
+
+if env | grep -q ^SNAPCI= then
+    gradle upload
+fi
